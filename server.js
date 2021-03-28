a4f5321e47d4f5324e76b2c4fb06b668c6a6b306
AhmadAbuyahya <Ahmadabuyahya998@gmail.com>
2021-03-28 17:14:28 +0300

    location 1st attempt

---
+++ server.js
@@ -0,0 +1,32 @@
+'use strict';
+// my port
+const PORT=3000;
+
+
+//dependencies
+const express =require('express');
+const cors = require('cors');
+const app=express();
+app.use(cors());
+
+// run on port
+app.listen(PORT, ()=> console.log(`App is running on Server on port: ${PORT}`));
+
+
+
+app.get('/location', handleLocation);
+
+
+// app.get('/restaurants', handleRestaurant);
+function handleLocation(request, response){
+  const getLocation = require('./Data/location.json');
+  const city = request.query.city;
+  let obj = {
+    search_query:city,
+    formatted_query: getLocation[0].display_name,
+    latitude: getLocation[0].lat,
+    longitude: getLocation[0].lon
+  };
+  response.send(obj);
+
+}
