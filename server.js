5293e8e588bbdf56f48cfe959c52fd91ed399bb6
AhmadAbuyahya <Ahmadabuyahya998@gmail.com>
2021-03-30 17:05:56 +0300

    fully finished lab 07

--- server.js
+++ server.js
@@ -16,12 +16,7 @@ app.use(cors());
 app.get('/location', locationHandler);
 app.get('/weather', handleWeather);
 app.get('/parks', handleParks);
-// app.get('/restaurants', restaurantHandler);
-// app.get('/places', placesHandler);
 
-// Express has an internal error handler.
-// so this means if you did not create your own.
-// express handler will respond
 app.use('*', notFoundHandler); // 404 not found url
 
 app.use(errorHandler);
@@ -32,90 +27,72 @@ function notFoundHandler(request, response) {
 function errorHandler(err, request, response, next) {
   response.status(500).send('something is wrong in server');
 }
-const myLocalLocations = {};
-
-function locationHandler(request, response) {
-  let city = request.query.city;
-  if (myLocalLocations[city]) {
-    console.log('2.from my local data');
-    response.send(myLocalLocations[city]);
-  } else {
-    let key = process.env.GEOCODE_API_KEY;
-    const url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json&limit=1`;
-    superagent.get(url).then(res=> {
-      const locationData = res.body[0];
-      const location = new Location(city, locationData);
-      myLocalLocations[city] = location;
-      response.send(location);
-    }).catch((err)=> {
-      console.log('ERROR IN LOCATION API');
-      console.log(err);
-    });
-  }
-}
-
 function Location(city, geoData) {
   this.search_query = city;
   this.formatted_query = geoData.display_name;
   this.latitude = geoData.lat;
   this.longitude = geoData.lon;
 }
-function WeatherObj(forecast, time) {
-  this.forecast = forecast;
-  this.time = time;
-}
-
-function handleWeather(request,response){
-  let key = process.env.WEATHER_API_KEY;
-  let lat = myLocalLocations.lat;
-  let lon = myLocalLocations.lon;
-  const url = `https://api.weatherbit.io/v2.0/forecast/daily??lat=${lat}&lon=${lon}&key=${key}`;
-  let weatherArr=[];
-  superagent.get(url).then(res=>{
-    res.body.data.forEach((obj)=>{
-      weatherArr.push(new WeatherObj(obj.weather.description, obj.valid_date));
-    });
-    response.send(weatherArr);
-
+function locationHandler(request, response) {
+  let city = request.query.city;
+  let key = process.env.GEOCODE_API_KEY;
+  const url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json&limit=1`;
+  superagent.get(url).then(res=> {
+    const locationData = res.body[0];
+    const location = new Location(city, locationData);
+    response.send(location);
   }).catch((err)=> {
     console.log('ERROR IN LOCATION API');
     console.log(err);
   });
 }
 
+
+
+function Weather(item) {
+  this.forecast = item.weather.description;
+  this.time = item.valid_date;
+}
+let weatherArr = [];
+
+function handleWeather(request,response){
+  let key = process.env.WEATHER_API_KEY;
+  let city = request.query.search_query;
+  const url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${key}`;
+  superagent.get(url).then(res=>{
+    res.body.data.map(item=>{
+      weatherArr.push(new Weather(item));
+    });
+    response.send(weatherArr);
+  }).catch((err)=> {
+    console.log('ERROR IN LOCATION API');
+    console.log(err);
+  });
+}
+function Park(name,address,fee,desc,url){
+  this.name=name;
+  this.address=address;
+  this.fee=fee;
+  this.desc=desc;
+  this.url=url;
+
+}
+let parkArr=[];
 function handleParks(request,response){
   let key=process.env.PARKS_API_KEY;
   let city = request.query.search_query;
-  const url = `https://developer.nps.gov/api/v1/parks?parkCode=${query}&api_key=${key}`;
+  const url = `https://developer.nps.gov/api/v1/parks?q=${city}&api_key=${key}&limit=5`;
   superagent.get(url).then(res=>{
-
-  })
-
+    res.body.data.map(element=>{
+      let park =new Park(element.fullName,element.addresses[0].city,element.entranceFees[0].cost,
+        element.description,element.url);
+      parkArr.push(park);
+    });
+    response.send(parkArr);
+  });
 }
 
-// function restaurantHandler(request, response) {
-//   // const url = 'https://developers.zomato.com/api/v2.1/geocode';
-// }
 
-// function Restaurant(entry) {
-//   this.restaurant = entry.restaurant.name;
-//   this.cuisines = entry.restaurant.cuisines;
-//   this.locality = entry.restaurant.location.locality;
-// }
-
-// function placesHandler(request, response) {
-
-//   // const lat = request.query.latitude;
-//   // const lng = request.query.longitude;
-//   // const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json`;
-
-// }
-
-// function Place(data) {
-//   this.name = data.text;
-//   this.type = data.properties.category;
-//   this.address = data.place_name;
-// }
 
 
 // Make sure the server is listening for requests
